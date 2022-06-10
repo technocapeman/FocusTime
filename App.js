154e255428a84483d0036fadc415719c5525e107
Kapilesh Pennichetty <kapilesh.pennichetty@gmail.com>
2022-06-10 12:25:04 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+import {colors} from './src/utils/colors';
+import {Focus} from './src/features/Focus';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Focus />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue
+  },
+});
---
+++ src/features/Focus.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-paper';
+import {colors} from '../utils/colors';
+
+export const Focus = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Focus Feature</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  text: {
+    color: colors.white
+  }
+})
